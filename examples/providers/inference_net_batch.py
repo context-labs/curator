6f79aa737909607784600c03d50e907990657cb8
Sam Heutmaker <samheutmaker@gmail.com>
2025-02-04 16:18:57 -0800

    update pricing

--- examples/providers/inference_net_batch.py
+++ examples/providers/inference_net_batch.py
@@ -1,5 +1,7 @@
 from bespokelabs import curator
 
+# View all available models at https://docs.inference.net/resources/models
+# inference.net currently only supports a 24h completion window
 llm = curator.LLM(
     model_name="meta-llama/llama-3.1-8b-instruct/fp-16", backend="inference.net", batch=True, backend_params={"max_retries": 1, "completion_window": "24h"}
 )
--- examples/providers/inference_net_online.py
+++ examples/providers/inference_net_online.py
@@ -1,5 +1,7 @@
 from bespokelabs.curator import LLM
 
+
+# View all available models at https://docs.inference.net/resources/models
 llm = LLM(model_name="meta-llama/llama-3.1-8b-instruct/fp-16", backend="inference.net", backend_params={"max_retries": 1})
 
 response = llm("Write a short story about a dog and a cat.")
