9ed806d399c12807a59748afd8a71946d504f9b0
TheDude <kartik4949@gmail.com>
2025-02-06 16:58:19 +0530

    ref: make details auto by default in openai multimodal

--- src/bespokelabs/curator/request_processor/online/base_online_request_processor.py
+++ src/bespokelabs/curator/request_processor/online/base_online_request_processor.py
@@ -148,8 +148,6 @@ class BaseOnlineRequestProcessor(BaseRequestProcessor, ABC):
                         "url": f"data:{mime_type};base64,{base64_content}",
                     },
                 }
-                if mime_type == "image/png":
-                    content["image_url"].update({"detail": "low"})
                 return content
 
         content = []
